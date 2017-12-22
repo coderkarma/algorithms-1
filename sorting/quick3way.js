7efb27fec0c75ca30a0b0ad8ac27d4741f4c34e5
ohdonghoon1128 <ohdonghoon1128@gmail.com>
2017-12-22 11:08:09 -0800

    3 way quick sort has better performance than quick sort

---
+++ sorting/quick3way.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const knuth = require('../fundamentals/Random/knuth');
+
+function sort(arr, comp) {
+    if(!(arr instanceof Array)) {
+        throw new Error('Illegal Argument Error: first arg must be array type');
+    }
+    knuth.shuffle(arr);
+    comp = comp || ((x, y) => x - y);
+
+    localSort(arr, 0, arr.length - 1, comp);
+}
+
+function localSort(arr, lo, hi, comp) {
+    if(lo >= hi) {
+        return;
+    }
+
+    let lt = lo;
+    let i = lo + 1;
+    let gt = hi;
+    let pivot = arr[lo];
+
+    while(i <= gt) {
+        let cmp = comp(pivot, arr[i]);
+
+        if(cmp > 0) {
+            swap(arr, lt++, i++);
+        } else if(cmp < 0) {
+            swap(arr, i, gt--);
+        } else {
+            i++;
+        }
+    }
+
+    localSort(arr, lo, lt - 1, comp);
+    localSort(arr, gt + 1, hi, comp);
+}
+
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+module.exports = sort;
