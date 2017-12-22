86881b34fbc0f94506c98368b3f779718233577d
ohdonghoon1128 <ohdonghoon1128@gmail.com>
2017-12-22 11:10:13 -0800

    Indexed Priority Queue - key in a IndexPQ can be changed and removed

---
+++ sorting/IndexPQ.js
@@ -0,0 +1,37 @@
+/*
+    incomplete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+'use strict';
+
+class IndexPQ {
+    constructor() {}
+
+    size() {}
+
+    isEmpty() {}
+
+    contains() {}
+
+    peekIndex() {}
+
+    peekKey() {}
+
+    remove() {}
+
+    changeKey() {}
+
+    decreaseKey() {}
+
+    increaseKey() {}
+
+    [Symbol.iterator]() {}
+
+    _swap() {}
+
+    _swim() {}
+
+    _sink() {}
+}
+
+module.exports = IndexPQ;
