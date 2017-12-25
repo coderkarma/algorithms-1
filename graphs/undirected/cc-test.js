f2582a3d8b5c1bc9e55c7edf6d19144172ee00d1
ohdonghoon1128 <ohdonghoon1128@gmail.com>
2017-12-25 14:24:43 -0800

    add more test

--- graphs/undirected/cc-test.js
+++ graphs/undirected/cc-test.js
@@ -12,5 +12,9 @@ graphReader(false, false)
         console.log('cc.count() ' + cc.count());
         console.log('cc.id(10) ' + cc.id(10));
         console.log('cc.size(2) ' + cc.size(2));
+        console.log('5 and 5 conntected: ' + cc.connected(5, 5));
+        console.log('5 and 6 conntected: ' + cc.connected(5, 6));
+        console.log('6 and 7 conntected: ' + cc.connected(6, 7));
+        console.log('{} and 3 connected: ' + cc.connected({}, 3));
     })
     .catch(console.log.bind(console));
