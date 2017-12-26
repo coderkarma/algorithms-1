ec922c8aac01bf15b9ac783036b62b31f550f5e3
ohdonghoon1128 <ohdonghoon1128@gmail.com>
2017-12-26 10:20:02 -0800

    Demo algorithm to show how to implement Non-recursive Depth First Search in general

--- graphs/directed/NonrecursiveDirectedDFS.js
+++ graphs/directed/NonrecursiveDirectedDFS.js
@@ -1,17 +1,20 @@
 'use strict';
 
+const Digraph = require('./unweighted/Digraph');
+
 class NonrecursiveDirectedDFS {
     constructor(g, ...sources) {
         if(!(g instanceof Digraph)) {
             throw new Error('Illegal Argument Error: first arg must be Digraph type');
         }
-        if(sources[0][Iterator]) {
-            sources = sources[0][Iterator]();
+
+        if(sources[0][Symbol.iterator]) {
+            sources = sources[0][Symbol.iterator]();
         }
 
-        this._visited = new Array(g.V()).fill(false);
-        this._vertices = [];
-        
+        this._visited = new Array(g.V);
+        this._reachableV = [];
+
         for(let source of sources) {
             if(!this._visited[source]) {
                 this._dfs(g, source);
@@ -20,15 +23,18 @@ class NonrecursiveDirectedDFS {
     }
 
     _dfs(g, source) {
-        const adj = new Array(g.V());
-        const n = adj.length;
+        this._validate(source);
+
+        const n = g.V;
+        const adj = new Array(n);
         for(let v = 0; v < n; v++) {
-            adj[i] = g.adj(v);
+            adj[v] = g.adj(v);
         }
-        const stack = new Array(n);
-        stack.push(s);
-        this._visited[s] = true;
-        this._vertices.push(s);
+
+        const stack = [];
+        stack.push(source);
+        this._visited[source] = true;
+        this._reachableV.push(source);
 
         while(stack.length) {
             const curr = stack[stack.length - 1];
@@ -36,26 +42,34 @@ class NonrecursiveDirectedDFS {
             const next = adj[curr].next();
             if(next.done) {
                 stack.pop();
-            } else {
-                if(!this._visited[next.value]) {
-                    stack.push(next.value);
-                    this._visited[next.value] = true;
-                    this._vertices.push(next.value);
-                }
+            } else if(!this._visited[next.value]) {
+                stack.push(next.value);
+                this._visited[next.value] = true;
+                this._reachableV.push(next.value);
             }
         }
-    
     }
 
     visited(v) {
+        this._validate(v);
+
         return this._visited[v];
     }
 
     /*
         return the collection of visited vertices form the source vertex
     */
-    vertices() {
-        return this._vertices[Symbol.iterator]();
+    reachableV() {
+        return this._reachableV[Symbol.iterator]();
+    }
+
+    _validate(v) {
+        if(!Number.isSafeInteger(v)) {
+            throw new Error(`Illegal Argument Error: (${typeof v} v) must be integer type`);
+        }
+        if(v < 0 || v >= this._visited.length) {
+            throw new Error(`Index Out Of Bound Error: ${v} must be between 0 <= v <= ${this._visited.length - 1}`);
+        }
     }
 }
 
